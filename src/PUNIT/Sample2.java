97f3b749a2c2f4691462f4e501234dd979dac014
suk c4 <suk c4@ADMINRG-TN51OJ3>
2022-07-26 21:16:07 +0530

    2nd commit

---
+++ src/PUNIT/Sample2.java
@@ -0,0 +1,13 @@
+package PUNIT;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("vihal");
+		
+		System.out.println("sample 2");
+
+	}
+
+}
