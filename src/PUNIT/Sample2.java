3a0ce1a532a79ce73a001e54c869e2379c8c6cb7
suk c4 <suk c4@ADMINRG-TN51OJ3>
2022-07-26 21:58:30 +0530

    4th commit

--- src/PUNIT/Sample2.java
+++ src/PUNIT/Sample2.java
@@ -5,7 +5,7 @@ public class Sample2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("vihal");
-		
+		System.out.println("vahini");
 		System.out.println("sample 2");
 
 	}
